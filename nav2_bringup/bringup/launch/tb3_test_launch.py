4f6e0135bf57a79495f17ecfe3aa78c41c1ee094
KvE <421606@student.saxion.nl>
2020-11-19 10:23:41 +0100

    Added BT's, test_bt_3 has the prefered logic and works with recoveries

--- nav2_bringup/bringup/launch/tb3_test_launch.py
+++ nav2_bringup/bringup/launch/tb3_test_launch.py
@@ -93,7 +93,7 @@ def generate_launch_description():
         'default_bt_xml_filename',
         default_value=os.path.join(
             get_package_share_directory('nav2_bt_navigator'),
-            'behavior_trees', 'test_bt.xml'),
+            'behavior_trees', 'test_bt_3.xml'),
         description='Full path to the behavior tree xml file to use')
 
     declare_autostart_cmd = DeclareLaunchArgument(
